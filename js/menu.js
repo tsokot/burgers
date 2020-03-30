596fe2350e34e320e5248a117c1f60ddde2a5275
Tsokot Alexandr <a.tsokot@gmail.com>
2020-03-30 20:28:21 +0300

    fix menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,19 @@
 function accordionMenu(){
-  const workers = document.querySelectorAll(".menu-acco__item");
+  const workers = document.querySelectorAll(".menu-acco__item"); // полуаем все li элементы
   const teamAccord = document.querySelector(".menu-acco");
+  const trigger = document.querySelector(".menu-acco__trigger");
+  const tarWidth = trigger.clientWidth; // ширина одной лишки (ссылки)
+  const windowWidth = document.documentElement.clientWidth; // ширина окна браузера
+  const closeMenuWidth = tarWidth * workers.length; // ширина закрытого слайдера (3 лишки)
+  const contentWidth = windowWidth - closeMenuWidth;
+  const tabletWidth = 768;
+  const contentDef = 500;
+ 
+
+console.log(workers.length);
+console.log(contentWidth);
+console.log(closeMenuWidth);
+
 
   teamAccord.addEventListener("click" , function(event){
       event.preventDefault(); // скидываем стандартное состояние (что бы не кидало страницу вверх / или не перенаправляло на другую)
@@ -12,13 +25,21 @@ function accordionMenu(){
               for (const iterator of workers) {
                   if(iterator !== worker){
                       iterator.classList.remove("menu-acco__item-active");
+                      iterator.lastElementChild.style.width = 0;
                   }
               }
 
               if(worker.classList.contains("menu-acco__item-active")){
                   worker.classList.remove("menu-acco__item-active");
+                  target.nextElementSibling.style.width = 0;
               }else{
                   worker.classList.add("menu-acco__item-active");
+                  if(windowWidth < tabletWidth){
+                    target.nextElementSibling.style.width = contentWidth +"px";
+                  } else {
+                        target.nextElementSibling.style.width = contentDef +"px";
+                  }
+                  
               }
       } 
       if(target.classList.contains("menu-acco__trigger-text")){
@@ -28,13 +49,21 @@ function accordionMenu(){
           for (const iterator of workers) {
             if(iterator !== worker){
                 iterator.classList.remove("menu-acco__item-active");
+                iterator.lastElementChild.style.width = 0;
             }
         }
 
         if(worker.classList.contains("menu-acco__item-active")){
             worker.classList.remove("menu-acco__item-active");
+            link.nextElementSibling.style.width = 0;
         }else{
             worker.classList.add("menu-acco__item-active");
+            if(windowWidth < tabletWidth){
+                link.nextElementSibling.style.width = contentWidth +"px";
+            } else {
+                link.nextElementSibling.style.width = contentDef + "px";
+            }
+            
         }
 
       }
